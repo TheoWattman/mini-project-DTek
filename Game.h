eefd56c6a4ccee518fc813d3b9cae92952de0675
TheoWattman <twattman@kth.se>
2024-02-18 21:31:41 +0100

    Added some comments

--- Game.h
+++ Game.h
@@ -8,8 +8,6 @@
 
 void updateDisplay();
 
-uint8_t programmeState = inGame;
-
 int game() {
 
     Pacman pacman;
@@ -43,20 +41,20 @@ int game() {
 
         displayScore(score);
 
-        if(pacman.x == pacman.targetX * GRIDSIZE && pacman.y == pacman.targetY * GRIDSIZE) { // If player is at target grid.
+        if(pacman.x == pacman.targetX * GRIDSIZE && pacman.y == pacman.targetY * GRIDSIZE) {    // If player is at target grid.
             
             checkPickup(pacman.targetX, pacman.targetY, &ghost, &score);
-            handleInput(&pacman);
+            handleInput(&pacman);                                                               
 
             if(!checkCollision(pacman.targetX + pacman.dirX, pacman.targetY + pacman.dirY)) {
                 pacman.targetX += pacman.dirX;
                 pacman.targetY += pacman.dirY;
             } else {
-                if(pacman.targetX == 24 && pacman.targetY == 9){    // At right teleporter
+                if(pacman.targetX == 24 && pacman.targetY == 9){                                 // At right teleporter
                     teleport(&pacman, 0, 9);
                     updateGhosts(&ghost, &pacman);
                 }
-                else if(pacman.targetX == 0 && pacman.targetY == 9){   // At left teleporter
+                else if(pacman.targetX == 0 && pacman.targetY == 9){                             // At left teleporter
                     teleport(&pacman, 24, 9);
                     updateGhosts(&ghost, &pacman);
                 } else {
@@ -65,7 +63,6 @@ int game() {
                 }
                 }
             }
-        }
 
         updateDisplay();
     }
--- display.h
+++ display.h
@@ -1,8 +1,52 @@
 #pragma once
 #include "stdint.h"
+#include <pic32mx.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
 
-void putPixel(int x, int y);
-void clearDisplay();
+// from the chipkit ref manual
+uint8_t spi_put_byte(uint8_t bVal);
+
+// Puts a pixel in the displayBuffer
+void putPixel(uint8_t x, uint8_t y);
+
+// Clears a pixel in the displayBuffer;
+void putNotPixel(uint8_t x, uint8_t y);
+
+// Displays a character.
 void displayChar(const uint8_t x, const uint8_t y, char character, const uint8_t *font);
+
+// Displays a String
 void displayString(const uint8_t x, const uint8_t y, char *character, const uint8_t *font);
-void displayBitarray(const int x, const int y, const uint8_t *byteArray);
+
+// Pauses the program for a few cycles
+void sleep(int cyc);
+
+// Displays array of bits at a given x and y coordinate.
+void displayBitarray(const uint8_t x, const uint8_t y, const uint8_t *byteArray);
+
+// Clears the display
+void clearDisplay(void);
+
+// from the chipkit ref manual
+void displayRow(uint8_t byteAmount, uint8_t * buffer);
+
+// Displays a line with a given x, y and length.
+void displayLine(int x, int y, int len);
+
+// Clears a line with a given x, y and length.
+void displayNotLine(int x, int y, int len) ;
+
+// Displays the score.
+void displayScore(int score);
+
+// Sends the displayBuffer to the display and updates it.
+// from the chipkit ref manual
+void updateDisplay();
+
+// from the chipkit ref manual
+void displayInit(void);
+
+// From github at https://github.com/is1200-example-projects/hello-display/blob/master/main.c#L271
+void SPIInit(void);
