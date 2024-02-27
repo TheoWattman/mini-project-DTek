c27cc4502950e4ae1e802a6a3f2c1d34fc9457e3
TheoWattman <twattman@kth.se>
2024-02-27 21:31:49 +0100

    comments

--- display.h
+++ display.h
@@ -12,7 +12,7 @@ uint8_t spi_put_byte(uint8_t bVal);
 void putPixel(uint8_t x, uint8_t y); // Theo
 
 // Clears a pixel in the displayBuffer;
-void putNotPixel(uint8_t x, uint8_t y);
+void putNotPixel(uint8_t x, uint8_t y); // Isac
 
 // Displays a character.
 void displayChar(const uint8_t x, const uint8_t y, char character, const uint8_t *font); // Theo
@@ -39,16 +39,16 @@ void displayLine(int x, int y, int len); // Theo
 void displayRect(int x, int y, int width, int height); // Theo
 
 // Clears a line with a given x, y and length.
-void displayNotLine(int x, int y, int len) ;
+void displayNotLine(int x, int y, int len); // Isac
 
 // Clears a rectangle with a given x, y, width and length.
 void clearRect(int x, int y, int width, int height); // Theo
 
 // Displays the score in the top left of the screen
-void displayScore(int score);
+void displayScore(int score); // Isac
 
 // converts an int to a string
-void numToString(int score, char* str);
+void numToString(int score, char* str); // Isac
 
 // Sends the displayBuffer to the display and updates it.
 // from the chipkit ref manual
