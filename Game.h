4bcbdd599360ee9263f91de0deacfa142e5d4558
TheoWattman <twattman@kth.se>
2024-02-27 20:54:06 +0100

    fixed bug

--- Game.h
+++ Game.h
@@ -44,7 +44,7 @@ int game(int * score) {
         // If player
         if(entityMapIsClear()) {
             initPacman(&pacman);
-            pacman.lives = 1;   
+            pacman.lives = 3;   
 
             initGhost(&ghosts[0], BLINKY);
             initGhost(&ghosts[1], INKY);
