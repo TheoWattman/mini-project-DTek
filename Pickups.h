796ae505b0552309437a490fb499dd4867ba1553
TheoWattman <twattman@kth.se>
2024-02-27 20:03:48 +0100

    comments

--- Pickups.h
+++ Pickups.h
@@ -6,8 +6,11 @@
 // Initializes all pickups in a bit array.
 void initEntityMap();
 
+// Draws pickups.
 void drawEntityMap(Pacman *pacman);
 
+// Checks if player is currently standing on a pickup.
 void checkPickup(int x, int y, Ghost *ghost, int* score);
 
+// Checks if player has cleared all pickups.
 int entityMapIsClear();
